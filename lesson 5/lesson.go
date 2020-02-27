32361ce6a59449412378a98e9d01e614ed950959
alpap <al.pap@live.com>
2020-02-27 17:03:23 +0100

    finished maps

---
+++ lesson 5/lesson.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// maps and struct
+	// map keys can only be types that can be tested for equality
+	// for example maps slices arrays cannot be map keys
+	scores := map[string]int{
+		"alex": 20,
+		"john": 40,
+		"kara": 50,
+	}
+	printOut(scores)
+	// creating maps // used when you dont have an initialization
+	makeScores := make(map[string]string)
+	makeScores = map[string]string{
+		"alex": "20",
+		"john": "40",
+		"kara": "50",
+	}
+	printOut(makeScores)
+	// manipulate the values of the map
+	// adding an element is at a random possition not in order of insertion
+	scores["brian"] = 44
+	printOut(scores["brian"])
+	printOut(scores)
+	// remove elements from a map
+	delete(scores, "brian")
+	printOut(scores)
+	// after deleting the value you can still ask to return it but it will return 0 or empty string
+	// this happens because it initializes a new variable of the selected type in that spot
+	// so watch out and validate your
+	printOut(scores["brian"])
+
+	// validation of the map
+	// ok will return true or false depending if the array was initialized
+	a, ok := scores["brian"]
+	printOut(a)
+	printOut(ok)
+	b, ok := scores["alex"]
+	printOut(b)
+	printOut(ok)
+
+	printOut(len(scores))
+
+	// maps as slices get passed by references
+
+	c := scores
+	printOut(c)
+	delete(c, "alex")
+	printOut(scores)
+	// if you want to avoid this from happening use the make declaration as shown above
+
+	printOut(scores)
+	printOut(makeScores)
+	delete(makeScores, "alex")
+	printOut(scores)
+	printOut(makeScores)
+
+	// struct
+	// check out the struct defined under the main function
+
+
+}
+
+type Person struct{
+	id int
+	name: string
+	surname: string
+}
+
+func printOut(value interface{}) {
+	fmt.Printf("%+v\n", value)
+}
