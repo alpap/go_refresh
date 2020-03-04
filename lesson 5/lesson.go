fdc452e4e076780898819295eb6271273c6bc37a
alpap <al.pap@live.com>
2020-03-04 07:46:19 +0100

    done with structs

--- lesson 5/lesson.go
+++ lesson 5/lesson.go
@@ -62,14 +62,35 @@ func main() {
 
 	// struct
 	// check out the struct defined under the main function
+	person := Person{
+		id:      3,
+		name:    "alex",
+		surname: "papad",
+	}
+	printOut(person)
+	printOut(person.name)
 
+	// anonymus structs
+	name := struct{ name string }{name: "john"}
+	printOut(name)
 
+	// structs are copied by value not by reference
+	person2 := person
+	person2.name = "jack"
+
+	printOut(person)
+
+	// copy by reference
+	person3 := &person
+	person3.name = "jack"
 }
 
-type Person struct{
-	id int
-	name: string
-	surname: string
+// if i want to export the struct i should name it with a capital letter as well as the fields as well if i want them to be populated outside the package
+
+type Person struct {
+	id      int
+	name    string
+	surname string
 }
 
 func printOut(value interface{}) {
