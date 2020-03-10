a84f4f9094adcf8dafdbcf4fd5ba5791b857ecbe
alpap <al.pap@live.com>
2020-03-10 20:07:13 +0100

    finished interfaces

--- lesson 11/lesson.go
+++ lesson 11/lesson.go
@@ -38,3 +38,29 @@ type WriterCloser interface {
 
 // type conversion of a interface to another
 // bwc:= writerCloser.(* bufferWriterCloser)
+
+// empty interface
+
+// if you want to work with multiple types without knowing the type close to generics
+type Empty interface{}
+
+// type case switches
+
+func typeSwitchExample() {
+	var i interface{} = 0
+	switch i.(type) {
+	case int:
+		println("i ia an integer")
+	case string:
+		println("i is a string")
+	default:
+		println("its something else")
+	}
+}
+
+// when writing an interface if we use a value type instace all the the methods have to have value type receivers (v Value)
+// on the other hand when i have a reference type receiver it parses all value and pointer type receivers (v * Value)
+
+// best practices
+// prefer many small interfaces
+// accept interfaces wherever possible in functions and methods
---
+++ lesson 12/lesson.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+
+}
+
+// basics
+func hallo(name, surname string) {
+	println("hallo " + name + " " + surname)
+}
