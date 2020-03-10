6965bd0fe27340287b02dccd47ba252cf726925b
alpap <al.pap@live.com>
2020-03-10 21:46:17 +0100

    finished routines

--- lesson 12/lesson.go
+++ lesson 12/lesson.go
@@ -1,24 +1,12 @@
 package main
 
-import "time"
-
 func main() {
 	// routines
 	go routine()
-	goSupportsClosures()
-	time.Sleep(100 * time.Millisecond)
 }
 
-// by putting go in front it spins off a green thread
-// this creates an abstraction of a routine
-// then its getting scheduled by the runtime
-func routine() {
-	println("hello")
-}
-
-func goSupportsClosures() {
-	var msg = "closure"
-	go func() {
-		println(msg)
-	}()
+func correctGoRoutine() {
+	go func(message string) {
+		println(message)
+	}("correct way")
 }
